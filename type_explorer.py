365979e36d98e9f9ff30b88eb25a4482d6ccf251
janimmelderim-svg <janim.melderim@gmail.com>
2026-02-16 22:43:15 +0200

    Initial commit: add type_explorer.py

---
+++ type_explorer.py
@@ -0,0 +1,38 @@
+#piešķiru 2 str vērtības
+text1="Janis"
+text2="galds"
+#pārbaudu vai str vērtības var saskaitīt
+print(text1+" "+text2)
+#piešķiru 2 int vērtības
+skaitlis1=3
+skaitlis2=7
+print(skaitlis1+skaitlis2)
+#pārbaudu vai int vērtības var saskaitīt
+print("skaitlis1"+ " "+"skaitlis2")
+print(int(skaitlis1)+6)
+#piešķiru 2 float vērtības
+float1=3.14
+float2=0.007
+#pārbaudu vai float vērtības var saskaitīt
+print(float1+float2)
+#Piešķiru 2 bool vērtības
+bool1=True
+bool2=False
+#Truth / false pārbaude
+print(bool1 and bool2)
+print(bool(""))
+print(bool(" "))
+print(bool("6197854"))
+print(bool1+bool1)
+print(bool1+bool1-bool1)
+print(bool1*bool2)
+print(bool1*10)
+print(10/(bool1+bool1))
+#citi interesanti gadījumi
+print(int(float1))
+print(1e3)
+#pārbaudu vai float vērtības var saskaitīt un vai rezultāts ir vienāds ar 0.3 rzultāts=false, jo rezultāts tieknoapaļots uz 0
+print(0.1+0.2==0.3)
+#true gadījums
+print(1+2==3)
+
